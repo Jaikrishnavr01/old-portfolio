2b05b85c6bc91ab3f20ba141ee0231be01433085
jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-07-19 23:32:10 +0530

    updated -link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -60,10 +60,10 @@ const Hero = () => {
                             {/* <div>Year</div>
                 <div>Experience</div> */}
                             <div className={css.iconContainer}>
-                                <a href="https://www.linkedin.com/in/jaikrishnavr/" target="_blank" rel="noopener noreferrer">
+                                <a href="https://www.linkedin.com/in/jaikrishnavr/" target="_self" rel="noopener noreferrer">
                                     <FontAwesomeIcon icon={faLinkedin} size="2x" />
                                 </a>
-                                <a href="https://github.com/Jaikrishnavr01" target="_blank" rel="noopener noreferrer">
+                                <a href="https://github.com/Jaikrishnavr01" target="_self" rel="noopener noreferrer">
                                     <FontAwesomeIcon icon={faGithub} size="2x" />
                                 </a>
                             </div>
